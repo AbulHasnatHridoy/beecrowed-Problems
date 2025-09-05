58c9386425dd0f0cd94624f98d5f7711b93ca2ff
Abul Hasnat Hridoy <hasnathridoy009@gmail.com>
2025-09-05 21:05:41 +0600

    1035

--- bee1020.cpp
+++ bee1020.cpp
@@ -1,14 +1,15 @@
-#include<stdio.h>
-int main(){
-int age;
-scanf("%d",&age);
-int year=age/365;
-int remaining=age%365;
-int month=remaining/30;
-int day=remaining%30;
-printf("%d ano(s)\n",year);
-printf("%d mes(es)\n",month);
-printf("%d dia(s)\n",day);
+#include <stdio.h>
+int main()
+{
+	int age;
+	scanf("%d",&age);
+	int year=age/365;
+	int remaining_days=age%365;
+	int month=remaining_days/30;
+	int days=remaining_days%30;
+	printf("%d ano(s)\n",year);
+	printf("%d mes(es)\n",month);
+	printf("%d dia(s)\n",days);
+	return 0;
 
-return 0;
 }
